6640efe0d7ec55d08a6f95e969b286f187b09a92
andrevinni <andre.vinicius.coelho@gmail.com>
2025-01-26 15:37:25 -0300

    Projeto criado

---
+++ src/main/java/com/cliquejah/ssjg/SsjgApplication.java
@@ -0,0 +1,13 @@
+package com.cliquejah.ssjg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsjgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsjgApplication.class, args);
+	}
+
+}
