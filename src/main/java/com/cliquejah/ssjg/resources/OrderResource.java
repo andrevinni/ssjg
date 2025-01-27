7758d23488978fb87f1957114ed5f61871c21b26
andrevinni <andre.vinicius.coelho@gmail.com>
2025-01-27 14:43:02 -0300

    Order, Instant, ISO 8601 part 2

---
+++ src/main/java/com/cliquejah/ssjg/resources/OrderResource.java
@@ -0,0 +1,39 @@
+package com.cliquejah.ssjg.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cliquejah.ssjg.entities.Order;
+import com.cliquejah.ssjg.services.OrderService;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderResource {
+	
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Order>> findAll(){
+		//Order u = new Order(1L, "Maria", "maria@gmail.com", "999999", "12345");
+		
+		List<Order> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		
+		Order obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+
+}
