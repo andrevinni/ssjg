1cc0286b2f433d2c968a002c413a281e151225a0
andrevinni <andre.vinicius.coelho@gmail.com>
2025-01-29 15:28:41 -0300

    Payment, one-to-one association

--- src/main/java/com/cliquejah/ssjg/config/TestConfig.java
+++ src/main/java/com/cliquejah/ssjg/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cliquejah.ssjg.entities.Category;
 import com.cliquejah.ssjg.entities.Order;
 import com.cliquejah.ssjg.entities.OrderItem;
+import com.cliquejah.ssjg.entities.Payment;
 import com.cliquejah.ssjg.entities.Product;
 import com.cliquejah.ssjg.entities.User;
 import com.cliquejah.ssjg.entities.enums.OrderStatus;
@@ -72,6 +73,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		p1.getCategories().add(cat2);
 		
--- src/main/java/com/cliquejah/ssjg/entities/Order.java
+++ src/main/java/com/cliquejah/ssjg/entities/Order.java
@@ -8,8 +8,8 @@ import java.util.Set;
 
 import com.cliquejah.ssjg.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -41,6 +42,9 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		super();
@@ -86,6 +90,14 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public Set<OrderItem> getItems() {
