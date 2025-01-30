c19344647f6944129094ec6b75384bc48bf10b7f
andrevinni <andre.vinicius.coelho@gmail.com>
2025-01-30 11:40:39 -0300

    Exception handling - findById

--- src/main/java/com/cliquejah/ssjg/services/UserService.java
+++ src/main/java/com/cliquejah/ssjg/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.cliquejah.ssjg.entities.User;
 import com.cliquejah.ssjg.repositories.UserRepository;
+import com.cliquejah.ssjg.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert (User obj) {
