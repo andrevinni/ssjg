843a3e978ba042f350d87d41252edc1ce91e63bf
André Vinícius Coelho da Silva <37165406+andrevinni@users.noreply.github.com>
2026-02-25 15:23:07 -0300

    Atualizar o SsjgApplication.java

--- src/main/java/com/casnav/testeapi/ssgnApi/SsjgApplication.java
+++ src/main/java/com/casnav/testeapi/ssgnApi/SsjgApplication.java
@@ -11,3 +11,24 @@ public class SsjgApplication {
 	}
 
 }
+
+```
+# Url de redirecionamento
+VITE_LOGIN_URL_DEV= https://sso.score.ctim.mb:9000/realms/score2-dev/account
+VITE_LOGIN_URL_PROD=https://login.empresa.com.br/sso?redirect_url=https://sistema.empresa.com.br/callback
+VITE_SSO_BASE_URL=https://sso.batatinha.com.br:3000
+VITE_OIDC_REALM=score2-dev
+VITE_OIDC_CLIENT_ID=app-ssjg
+VITE_OIDC_REDIRECT_URI=http://localhost:5173/callback
+
+# Define o ambiente ativo
+VITE_ENV=developement
+
+VITE_APP_VERSAO=$npm_package_version
+
+VITE_APP_OAUTH2_URL= https://sso.score.ctim.mb:9000/realms/score2-dev 
+VITE_APP_OAUTH2_CLIENT_ID=app-ssjg
+VITE_APP_OAUTH2_CLIENT_SECRET=mSHgJZG5NSI472axSmDs3KJTQnCPM6A9
+
+VITE_APP_API_BASEURL=http://10.5.115.46:8081/login/getAuthorities
+```
