c0af3696877aca640960cba7ac5d2fdd0ca995e1
André Vinícius Coelho da Silva <37165406+andrevinni@users.noreply.github.com>
2026-02-26 08:26:46 -0300

    Atualizar o SsjgApplication.java

--- src/main/java/com/casnav/testeapi/ssgnApi/SsjgApplication.java
+++ src/main/java/com/casnav/testeapi/ssgnApi/SsjgApplication.java
@@ -12,3 +12,13 @@ public class SsjgApplication {
 
 }
 
+
+``` proxy=http://07972427755:SENHA@proxy-1dn.mb.6060
+https-proxy=http://07972427755:SENHA@proxy-1dn.mb.6060/
+noproxy=.mb
+registry=http://nexus.casnav.mb:8081/repository/npm-group
+;@casnav-avantis:registry=http://nexus.casnav.mb:8081/repository/avantis-npm
+@casnav-avantis:registry=http://nexus.casnav.mb:8081/repository/npm-casnav
+
+//nexus.casnav.mb:8081/repository/:_authToken=NpmToken.a2f86159-08d3-30bc-80ce-03b71b2ba5f0 ````
+
